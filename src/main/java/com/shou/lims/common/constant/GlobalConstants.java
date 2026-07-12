38ea4bbf31b81300506c4eeab9fa6f79dfeb1c76
YHL <1241171248@qq.com>
2026-07-12 22:33:42 +0800

    feat: add Phone validator, CacheService, GlobalConstants

---
+++ src/main/java/com/shou/lims/common/constant/GlobalConstants.java
@@ -0,0 +1,10 @@
+package com.shou.lims.common.constant;
+
+public final class GlobalConstants {
+
+    private GlobalConstants() {}
+
+    public static final String REDIS_REFRESH_TOKEN_PREFIX = "refresh:";
+    public static final String REDIS_BLACKLIST_PREFIX = "blacklist:";
+    public static final String REDIS_RSA_KEY_PREFIX = "rsa:";
+}
