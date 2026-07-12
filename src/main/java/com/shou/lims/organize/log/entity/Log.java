ad58309aeaf023339b21915245f07198bd1a1634
YHL <1241171248@qq.com>
2026-07-12 22:46:27 +0800

    feat: add all organize module entities (User, Role, Permission, Dept, Menu, Log)

---
+++ src/main/java/com/shou/lims/organize/log/entity/Log.java
@@ -0,0 +1,27 @@
+package com.shou.lims.organize.log.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("sys_log")
+public class Log {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private Long userId;
+    private String username;
+    private String module;
+    private String action;
+    private String method;
+    private String params;
+    private String result;
+    private String ip;
+    private Integer duration;
+    private Integer status;
+    private LocalDateTime createTime;
+    private Integer isDelete;
+}
---
+++ src/main/java/com/shou/lims/organize/menu/entity/Menu.java
@@ -0,0 +1,24 @@
+package com.shou.lims.organize.menu.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.shou.lims.common.entity.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("sys_menu")
+public class Menu extends BaseEntity {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private Long parentId;
+    private String name;
+    private String path;
+    private String component;
+    private String icon;
+    private Integer sortOrder;
+    private Integer hidden;
+    private Integer status;
+}
