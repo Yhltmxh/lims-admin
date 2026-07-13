53d3b9e36c0c33b87d9b84cb7c534da846577274
YHL <1241171248@qq.com>
2026-07-14 01:26:26 +0800

    chore: 清理临时文件

--- src/test/java/com/shou/lims/GenPassword.java
+++
@@ -1,7 +0,0 @@
-package com.shou.lims;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-public class GenPassword {
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("123456"));
-    }
-}
